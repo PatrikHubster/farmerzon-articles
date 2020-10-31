030e0d48204b59c65d3adbbc9a6ab3bd9284ea38
patrikhuber <patrihub@icloud.com>
2020-10-31 18:09:26 +0100

    Removed old name of person id because it is already in the base model.

--- FarmerzonArticlesDataTransferModel/PersonOutput.cs
+++ FarmerzonArticlesDataTransferModel/PersonOutput.cs
@@ -2,8 +2,6 @@
 {
     public class PersonOutput : BaseModelOutput
     {
-        public long PersonId { get; set; }
-
         public string UserName { get; set; }
         public string NormalizedUserName { get; set; }
     }
